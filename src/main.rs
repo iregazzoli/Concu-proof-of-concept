ca47961154e3b2000b64a749d72331b2a5c53a6a
Regazzoli Ignacio <ignacioregazzoli@gmail.com>
2024-05-30 09:46:57 -0300

    code refactor

--- src/main.rs
+++ src/main.rs
@@ -5,106 +5,108 @@ use actix::prelude::*;
 use ice_cream_shop::{AddIceCream, AddOrder, IceCreamShop};
 use serde_json::from_slice;
 use shared::order::Order;
-use tokio::io::AsyncBufReadExt;
-use tokio::io::AsyncWriteExt;
-use tokio::io::BufReader;
-use tokio::net::TcpListener;
+use std::net::SocketAddr;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
 
 #[actix_rt::main]
 async fn main() {
-    // Crear el IceCreamShop
+    let ice_cream_shop = setup_ice_cream_shop().await;
+    start_server(ice_cream_shop).await;
+}
+
+async fn setup_ice_cream_shop() -> Addr<IceCreamShop> {
     let ice_cream_shop = IceCreamShop::new().start();
+    add_initial_ice_creams(&ice_cream_shop).await;
+    ice_cream_shop
+}
 
-    // Agregar algunos helados iniciales
+async fn add_initial_ice_creams(ice_cream_shop: &Addr<IceCreamShop>) {
+    add_ice_cream(ice_cream_shop, "Vanilla", 10).await;
+    add_ice_cream(ice_cream_shop, "Chocolate", 20).await;
+}
+
+async fn add_ice_cream(ice_cream_shop: &Addr<IceCreamShop>, flavor: &str, quantity: u32) {
     let res = ice_cream_shop
         .send(AddIceCream {
-            flavor: "Vanilla".to_string(),
-            quantity: 10,
+            flavor: flavor.to_string(),
+            quantity,
         })
         .await;
 
     match res {
-        Ok(_) => println!("Helado de vainilla agregado exitosamente"),
-        Err(_) => println!("Falló al agregar helado de vainilla"),
+        Ok(_) => println!("{} ice cream added successfully", flavor),
+        Err(_) => println!("Failed to add {} ice cream", flavor),
     }
+}
 
-    let res = ice_cream_shop
-        .send(AddIceCream {
-            flavor: "Chocolate".to_string(),
-            quantity: 20,
-        })
-        .await;
-
-    match res {
-        Ok(_) => println!("Helado de chocolate agregado exitosamente"),
-        Err(_) => println!("Falló al agregar helado de chocolate"),
-    }
-
-    // Iniciar el servidor TCP
+async fn start_server(ice_cream_shop: Addr<IceCreamShop>) {
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
-
-    println!("\nEsperando conexiones!\n");
-
+    println!("\nWaiting for connections!\n");
     let mut next_client_id = 1;
 
     loop {
         let (mut stream, addr) = listener.accept().await.unwrap();
-        println!("[{:?}] Cliente conectado", addr);
+        println!("[{:?}] Client connected", addr);
 
-        // Asignar un ID al cliente
         let client_id = next_client_id;
         next_client_id += 1;
 
-        // Enviar el ID al cliente
         let id_message = format!("{}\n", client_id);
         stream.write_all(id_message.as_bytes()).await.unwrap();
 
         let mut reader = BufReader::new(stream);
+        handle_client(&ice_cream_shop, &mut reader, addr, client_id).await;
+    }
+}
 
-        let mut line = String::new();
-        while reader.read_line(&mut line).await.unwrap() > 0 {
-            match from_slice::<Order>(line.trim().as_bytes()) {
-                Ok(order) => {
-                    println!(
-                        "[{:?}] Received order: {} of {} from client {}",
-                        addr, order.quantity, order.flavor, client_id
-                    );
+async fn handle_client(
+    ice_cream_shop: &Addr<IceCreamShop>,
+    reader: &mut BufReader<TcpStream>,
+    addr: SocketAddr,
+    client_id: usize,
+) {
+    let mut line = String::new();
+    while reader.read_line(&mut line).await.unwrap() > 0 {
+        match from_slice::<Order>(line.trim().as_bytes()) {
+            Ok(order) => {
+                println!(
+                    "[{:?}] Received order: {} of {} from client {}",
+                    addr, order.quantity, order.flavor, client_id
+                );
 
-                    let res = ice_cream_shop.send(AddOrder { order }).await;
-                    match res {
-                        Ok(_) => {
-                            println!(
-                                "[{:?}] Order from client {} enqueue successfully \n",
-                                addr, client_id
-                            );
-                            // Enviar una respuesta al cliente
-                            let response = "Order processed successfully\n";
-                            reader
-                                .get_mut()
-                                .write_all(response.as_bytes())
-                                .await
-                                .unwrap();
-                        }
-                        Err(_) => {
-                            println!(
-                                "[{:?}] Failed to process order from client {} \n",
-                                addr, client_id
-                            );
-                            // Enviar una respuesta al cliente
-                            let response = "Failed to process order\n";
-                            reader
-                                .get_mut()
-                                .write_all(response.as_bytes())
-                                .await
-                                .unwrap();
-                        }
+                let res = ice_cream_shop.send(AddOrder { order }).await;
+                match res {
+                    Ok(_) => {
+                        println!(
+                            "[{:?}] Order from client {} enqueue successfully \n",
+                            addr, client_id
+                        );
+                        let response = "Order processed successfully\n";
+                        reader
+                            .get_mut()
+                            .write_all(response.as_bytes())
+                            .await
+                            .unwrap();
+                    }
+                    Err(_) => {
+                        println!(
+                            "[{:?}] Failed to process order from client {} \n",
+                            addr, client_id
+                        );
+                        let response = "Failed to process order\n";
+                        reader
+                            .get_mut()
+                            .write_all(response.as_bytes())
+                            .await
+                            .unwrap();
                     }
                 }
-                Err(e) => {
-                    println!("[{:?}] Failed to deserialize order: {}", addr, e);
-                }
             }
-            line.clear();
+            Err(e) => {
+                println!("[{:?}] Failed to deserialize order: {}", addr, e);
+            }
         }
+        line.clear();
     }
 }
